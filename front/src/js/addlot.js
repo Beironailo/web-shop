704d34b63b95705fab3caadd72dd00c60cd8866c
Tufra <60471036+Tufra@users.noreply.github.com>
2021-06-27 17:18:20 +0300

    Update addlot.js

--- front/src/js/addlot.js
+++ front/src/js/addlot.js
@@ -1,20 +1,24 @@
-function addlot(param,container){
+function addLot(param, container) {
 
-  let lotContainer=document.querySelector(container);
-  let lot=document.createElement('div');
-  lot.ClassName="lot";
+  let lotContainer = document.querySelector(container);
+  let lot = document.createElement('div');
+  lot.ClassName = "lot";
 
-  let imgTag="<img src = \"" +  param.src + "\"class = \"lot_img\">";
+  let imgTag="<img src = \"" +  param.src + "\" class = \"lot_img\">";
   let nameTag = "<div class = \"name_text\">" + param.name + "</div>";
   let price = "<div >" + param.price + "</div>";
-  let buttonsCont=createElement('div');
-  let buttonW=createElement('button');
+
+  let buttonsCont = document.createElement('div');
+
+  let buttonW = document.createElement('button');
   buttonW.class = "white_button";
   buttonW.setAttribute("data-itemid", param.id);
-  let buttonP = createElement('button');
-  buttonW.class = "purple_button";
-  buttonW.setAttribute("data-itemid", param.id);
-  buttonsCont.innerHTML += buttonP+buttonW;
+
+  let buttonP = document.createElement('button');
+  buttonP.class = "purple_button";
+  buttonP.setAttribute("data-itemid", param.id);
+
+  buttonsCont.innerHTML += buttonP + buttonW;
 
   lot.innerHTML += imgTag + nameTag + price + buttonsCont;
   lotContainer.innerHTML += lot;
