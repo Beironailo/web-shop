1a6c591ccbd1d08687e6af137f76c8b93528fa7a
SShakirov <shaman531@mail.ru>
2021-06-27 17:15:15 +0300

    +addlot script
    функция добавления лота

--- front/src/js/addlot.js
+++ front/src/js/addlot.js
@@ -1,13 +1,21 @@
-function addlot(param){
+function addlot(param,container){
 
-  let lotContainer=document.querySelector(param.container);
+  let lotContainer=document.querySelector(container);
   let lot=document.createElement('div');
   lot.ClassName="lot";
-  lot.id=param.id;
-  let imgTag="<img src= \""+ param.src +"\"class=\"lot_img\">";
-  let nameTag = "<div class=\"name_text\">" +param.name +"</div>";
-  let price = "<div >"+param.price+"</div>";
-  let buttons="<div><button type=\"button\" name=\"purple_button\"  class=\"purple_button\">Купить</button><button type=\"button\" name=\"white_button\" class=\"white_button\">Подробнее</button></div>";
-  lot.innerHTML += imgTag+nameTag+price+buttons;
-  lotContainer.innerHTML +=lot;
+
+  let imgTag="<img src = \"" +  param.src + "\"class = \"lot_img\">";
+  let nameTag = "<div class = \"name_text\">" + param.name + "</div>";
+  let price = "<div >" + param.price + "</div>";
+  let buttonsCont=createElement('div');
+  let buttonW=createElement('button');
+  buttonW.class = "white_button";
+  buttonW.setAttribute("data-itemid", param.id);
+  let buttonP = createElement('button');
+  buttonW.class = "purple_button";
+  buttonW.setAttribute("data-itemid", param.id);
+  buttonsCont.innerHTML += buttonP+buttonW;
+
+  lot.innerHTML += imgTag + nameTag + price + buttonsCont;
+  lotContainer.innerHTML += lot;
 }
