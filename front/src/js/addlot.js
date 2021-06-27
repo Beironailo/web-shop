b212d9eefc9394075e620dce41745a9a7993349d
SShakirov <shaman531@mail.ru>
2021-06-27 16:46:26 +0300

    Create addlot.js

---
+++ front/src/js/addlot.js
@@ -0,0 +1,12 @@
+function addlot(param){
+
+  let lotContainer=document.querySelector(param.container);
+  let lot=document.createElement('div');
+  lot.ClassName="lot";
+  let imgTag="<img src= \""+ param.src +"\"class=\"lot_img\">";
+  let nameTag = "<div class=\"name_text\">" +param.name +"</div>";
+  let price = "<div >"+param.price+"</div>";
+  let buttons="<div><button type=\"button\" name=\"purple_button\"  class=\"purple_button\">Купить</button><button type=\"button\" name=\"white_button\" class=\"white_button\">Подробнее</button></div>";
+  lot.innerHTML += imgTag+nameTag+price+buttons;
+  lotContainer.innerHTML +=lot;
+}
